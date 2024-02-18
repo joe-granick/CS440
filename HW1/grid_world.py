d51e999d8d59c19a101c62c9ec234a8e8bffe79d
yingfan-he <yingfanhe@gmail.com>
2024-02-17 19:18:51 -0500

    Cleaned up grid_world code and added more comments

--- HW1/grid_world.py
+++ HW1/grid_world.py
@@ -1,80 +1,102 @@
 import random
 
-"""class to initiliaze grid with start stae goal state and obstacles"""
 class GridWorld:
     def __init__(self, rows, cols):
         """
-        
-        initialize RxC size grid as the search space
-        
-        path creates a matrix with all cells initiialized to True representing
-        an open path 
-            
-        """
+        Initializes a grid with specified rows and columns.
 
+        The grid is represented by the 'path' matrix with all cells initially set to False,
+        indicating a blocked path.
+
+        The 'visited' set is used to keep track of visited cells during maze generation.
+        """
         self.rows = rows
         self.cols = cols
+        self.path = [[False for x in range(self.cols)] for y in range(self.rows)]
+        self.visited = set()
 
+    def block_path(self, x, y):
+        """
+        Blocks a cell in the grid by setting its coordinates to False.
 
-        self.path = [
-               [False for x in range(self.cols)]
-                for y in range(self.rows)
-               ]
-
-    
-    def block_path(self,x, y):
-        """helper function, blocks a cell by setting its coordinates to False"""
+        :param x: x-coordinate of the cell.
+        :param y: y-coordinate of the cell.
+        """
         self.path[y][x] = False
 
     def valid_move(self, x, y):
-        return 0 <= x < self.cols and 0 <= y < self.rows and (x, y) not in visited 
+        """
+        Checks if a move to the specified coordinates (x, y) is valid.
+
+        :param x: x-coordinate of the cell.
+        :param y: y-coordinate of the cell.
+        :return: True if the move is valid, False otherwise.
+        """
+        return 0 <= x < self.cols and 0 <= y < self.rows and (x, y) not in self.visited
 
     def generate_moves(self, x, y):
-        return[
-                (x-1, y), (x+1, y),
-                (x, y-1), (x, y+1)
-                ]
+        """
+        Generates possible moves from a given cell.
+
+        :param x: x-coordinate of the cell.
+        :param y: y-coordinate of the cell.
+        :return: List of possible moves.
+        """
+        return [(x - 1, y), (x + 1, y), 
+                (x, y - 1), (x, y + 1)]
 
     def dfs(self, x, y):
-        """depth first search implementation for creating the maze"""
-        
-        if not valid_move(x,y): return
+        """
+        Depth-first search implementation for creating the maze.
 
-        visited.add(x, y)
+        :param x: x-coordinate of the cell.
+        :param y: y-coordinate of the cell.
+        """
+        if not self.valid_move(x, y):
+            return
 
+        self.visited.add((x, y))
 
-        # leaves cell unvisited 30% prob
-        if random.random() < 0.3: 
+        # Leaves cell unvisited with a 30% probability
+        if random.random() < 0.3:
             return
 
         self.path[y][x] = True
-        moves = generate_moves(x, y)
+        moves = self.generate_moves(x, y)
         random.shuffle(moves)
 
-        for x, y in moves:
-            dfs(x, y)
+        for nx, ny in moves:
+            self.dfs(nx, ny)
 
     def create_maze(self):
         """
-        Generates maze on grid by starting at a random point and perfroming a DFS
-        for all cells and randomly blocks cells with a 30% probability
+        Generates a maze on the grid by starting at a random point and recursively calls DFS
+        for all cells, randomly blocking cells with a 30% probability.
         """
-
-        start_x = random.randint(0,self.cols-1)
-        start_y = random.randint(0,self.rows-1)
-        visited = set()
-        
-        dfs(start_x, start_y)
+        start_x = random.randint(0, self.cols - 1)
+        start_y = random.randint(0, self.rows - 1)
+        self.visited = set()
+        self.dfs(start_x, start_y)
 
     def print_grid(self):
+        """
+        Prints the grid, displaying 'O' for open path and 'X' for blocked path.
+        """
         for row in range(self.rows):
             for col in range(self.cols):
-                if self.path[row][col]: print 'O'
-                else: print 'X'
-            print
+                if self.path[row][col]:
+                    print('O', end=' ')
+                else:
+                    print('X', end=' ')
+            print()
 
     def main(self):
+        """
+        Main function to create an instance of GridWorld, generate a maze, and print the grid.
+        """
+        grid = GridWorld(101, 101)
+        grid.create_maze()
+        grid.print_grid()
 
-        grid = GridWorld(101,101)
-        grid.create_maze
-
+if __name__ == "__main__":
+    GridWorld().main()
